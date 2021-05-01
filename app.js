d228e4b4ae88fbf5a6de6c635832f390c9127966
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-01 09:42:01 +0300

    use body-parser

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const PORT = 8888;
 // APIs
 const chartsAPIRoute = require("./routes/api/chartsAPI"); // get the router for api
 
+// Use Body Parser
+// to parse elements in Request Form
+const bodyParser = require("body-parser");
+// relate body parser with express server (router)
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 // Using API Routers
 app.use("/api/charts", chartsAPIRoute);
 
