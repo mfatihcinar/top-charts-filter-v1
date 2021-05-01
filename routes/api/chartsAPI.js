f0cf0240dafbdd7488a94c7dcb5aec058c0b8a5c
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-01 09:45:00 +0300

    create and initialize Charts API

---
+++ routes/api/chartsAPI.js
@@ -0,0 +1,15 @@
+/*
+    This is the CHARTS API
+    All the analysis and request is made here.
+    This API will make an HTTP request to AppAnnie API
+    Get all the applications that are trending
+    and calculates which ones are new and which are not
+    Result will be recently-developed trending applications or games
+*/
+const express = require("express");
+
+const app = express();
+// create an express instance
+
+
+
