ecb9fb708c2fe760401eb2aea3db24eb4e8ce415
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-01 10:52:22 +0300

    create URL Data seperately

---
+++ data/URL.js
@@ -0,0 +1,7 @@
+/*
+    This file carries the URL, i.e. APP Annie API for HTTP requests
+*/
+
+const theURL = "https://www.appannie.com/ajax/v2/query?query_identifier=table_change(top_apps$ios_table_free)";
+
+module.exports = theURL;
