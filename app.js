b7f9fa1afc16d7356a75b3c408b9278914703d5f
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-05-01 13:27:19 +0300

    configure view engines

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 
 const PORT = 8888;
 
+/* CONFIGURE VIEW ENGINES */
+app.set("view engine", "pug");
+// say that we want to use pug template engine
+app.set("views", "views");
+// means that our pug views is located in views folder
+
 // APIs
 const chartsAPIRoute = require("./routes/api/chartsAPI"); // get the router for api
 
@@ -27,3 +33,4 @@ app.get("/", (request, response, next) => {
     const OKAY = 200;
     response.status(OKAY).send("Server is ON! :)");
 }); 
+
